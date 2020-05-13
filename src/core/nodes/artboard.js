9dcaf8b165f3859df78d8aa12672c6f44446f9f6
Grant Skinner <info@gskinner.com>
2020-05-13 13:46:30 -0600

    Add header comment to source code.

--- src/core/nodes/artboard.js
+++ src/core/nodes/artboard.js
@@ -1,3 +1,14 @@
+/*
+Copyright 2020 Adobe
+All Rights Reserved.
+
+NOTICE: Adobe permits you to use, modify, and distribute this file in
+accordance with the terms of the Adobe license agreement accompanying
+it. If you have received this file from a source other than Adobe,
+then your use, modification, or distribution of it requires the prior
+written permission of Adobe. 
+*/
+
 const xd = require("scenegraph");
 const utils = require("../utils");
 const NodeUtils = require("../nodeutils");
--- src/core/pubspec.js
+++ src/core/pubspec.js
@@ -1,3 +1,14 @@
+/*
+Copyright 2020 Adobe
+All Rights Reserved.
+
+NOTICE: Adobe permits you to use, modify, and distribute this file in
+accordance with the terms of the Adobe license agreement accompanying
+it. If you have received this file from a source other than Adobe,
+then your use, modification, or distribution of it requires the prior
+written permission of Adobe. 
+*/
+
 // this is a basic tool for parsing and validating specific elements of a
 // pubspec.yaml file. It is not a YAML processor!
 
