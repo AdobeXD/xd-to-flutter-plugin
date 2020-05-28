736f455748ce9b038a0ed1ac91e0b41ad186ad52
Grant Skinner <info@gskinner.com>
2020-05-28 11:50:38 -0600

    Rewrite Ellipse to extend Rectangle.

--- src/core/nodes/ellipse.js
+++ src/core/nodes/ellipse.js
@@ -9,38 +9,19 @@ then your use, modification, or distribution of it requires the prior
 written permission of Adobe. 
 */
 
-const xd = require("scenegraph");
-
 const $ = require("../../utils/utils");
-const NodeUtils = require("../../utils/nodeutils");
-const PropType = require("../proptype");
+const { Rectangle } = require("./rectangle");
 const { getDecorationParam } = require("../serialize/decorations");
-const { Parameter, ParameterRef } = require("../parameter");
 
-class Ellipse {
-	constructor(xdNode) {
-		this.xdNode = xdNode;
-		this.parameters = {};
-
-		let hasImageFill = (this.xdNode.fill instanceof xd.ImageFill);
-		let fillParam = new Parameter(this, hasImageFill ? "ImageFill" : "Color", "fill", this.xdNode.fill);
-		this.parameters["fill"] = new ParameterRef(fillParam, true,
-			hasImageFill ? NodeUtils.getProp(this.xdNode, PropType.IMAGE_PARAM_NAME) : null);
-
-		let strokeParam = new Parameter(this, "Color", "stroke", this.xdNode.stroke);
-		this.parameters["stroke"] = new ParameterRef(strokeParam, true);
-
-		let strokeEnableParam = new Parameter(this, "Boolean", "strokeEnabled", this.xdNode.strokeEnabled);
-		this.parameters["strokeEnabled"] = new ParameterRef(strokeEnableParam, true);
-	}
-
-	toString(serializer, ctx) {
+class Ellipse extends Rectangle {
+	// An ellipse is just a rectangle that calculates its size differently
+	// and always uses a decoration because it always has rounded corners.
+	_serialize(serializer, ctx) {
 		let o = this.xdNode;
 		let w = $.fix(o.radiusX * 2), h = $.fix(o.radiusY * 2);
 		let c = getDecorationParam(o, serializer, ctx, this.parameters);
 		return `Container(width: ${w}, height: ${h}, ${c})`;
 	}
-
 }
 exports.Ellipse = Ellipse;
  
