d900262e20e077aefe62fd0d07a997af2d04a267
Grant Skinner <info@gskinner.com>
2020-06-03 17:00:10 -0600

    Remove support for Object Blur

--- src/core/decorators/blur.js
+++ src/core/decorators/blur.js
@@ -24,8 +24,8 @@ class Blur extends NodeDecorator {
 				ctx.log.warn("Blur is currently only supported on rectangles and ellipses.", xdNode);
 				return;
 			}
-			if (!blur.isBackgroundEffect && node.responsive) {
-				ctx.log.warn("Object blur is not supported in responsive layouts.", xdNode);
+			if (!blur.isBackgroundEffect) {
+				ctx.log.warn("Object blur is not supported.", xdNode);
 				return;
 			}
 			if (blur.isBackgroundEffect && Math.round(xdNode.blur.brightnessAmount) !== 0) {
@@ -40,29 +40,7 @@ class Blur extends NodeDecorator {
 		let xdNode = this.node.xdNode, blur = xdNode.blur;
 		let clipType = xdNode instanceof xd.Rectangle ? "ClipRect" : "ClipOval";
 		let filterParam = getImageFilterParam(blur, serializer, ctx);
-		
-		if (blur.isBackgroundEffect) {
-			// background blur.
-			let str = `BackdropFilter(${filterParam}child: ${nodeStr}, )`;
-			return `${clipType}(child: ${str}, )`;
-		}
-		
-		// object blur.
-		// TODO: GS: maybe just remove object blur? It's pretty hacky, and may not be possible to support with responsive layout.
-		let bounds = xdNode.localBounds;
-		let blurRadius = blur.blurAmount * 1.5;
-		let bx = $.fix(bounds.x - blurRadius);
-		let by = $.fix(bounds.y - blurRadius);
-		let bw = $.fix(bounds.width + blurRadius * 2);
-		let bh = $.fix(bounds.height + blurRadius * 2);
-		return "Stack(overflow: Overflow.visible, children: <Widget>[" +
-			`${nodeStr}, ` +
-			`Positioned(left: ${bx}, top: ${by}, width: ${bw}, height: ${bh}, ` +
-				`child: ${clipType}(child:BackdropFilter(${filterParam}` +
-					"child: Container(color: const Color(0x00000000)), )," +
-				"), " +
-			"), " + 
-		"], )";
+		return `${clipType}(child: BackdropFilter(${filterParam}child: ${nodeStr}, ), )`;
 	}
 }
 exports.Blur = Blur;
