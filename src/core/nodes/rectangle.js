7d2d6c5bf987d1eddc0b6fea9235eaf78c5ae66c
Grant Skinner <info@gskinner.com>
2020-06-03 16:56:53 -0600

    Move decorations code into Rectangle

--- src/core/nodes/rectangle.js
+++ src/core/nodes/rectangle.js
@@ -14,9 +14,13 @@ const xd = require("scenegraph");
 const $ = require("../../utils/utils");
 const { ExportNode } = require("./exportnode");
 const NodeUtils = require("../../utils/nodeutils");
-const { getColorOrDecorationParam, getDecorationParam } = require("../serialize/decorations");
 const PropType = require("../proptype");
 const { ParamType } = require("../parameter");
+
+const { getColor } = require("../serialize/colors");
+const { getAssetImage } = require("../serialize/core");
+const { getBoxFit } = require("../serialize/layout");
+const { getGradientParam } = require("../serialize/gradients");
 
 class Rectangle extends ExportNode {
 	static create(xdNode, ctx) {
@@ -42,10 +46,140 @@ class Rectangle extends ExportNode {
 		let o = this.xdNode, isRect = o instanceof xd.Rectangle;
 		let w = $.fix(isRect ? o.width : o.radiusX * 2);
 		let h = $.fix(isRect ? o.height : o.radiusY * 2);
-		let c = isRect ? getColorOrDecorationParam(o, serializer, ctx, this.parameters) : 
-			getDecorationParam(o, serializer, ctx, this.parameters);
+		let c = isRect ? _getColorOrDecorationParam(o, serializer, ctx, this.parameters) : 
+			_getDecorationParam(o, serializer, ctx, this.parameters);
 		return `Container(width: ${w}, height: ${h}, ${c})`;
 	}
 
 }
 exports.Rectangle = Rectangle;
+
+
+/** BOXDECORATION */
+function _getColorOrDecorationParam(xdNode, serializer, ctx, parameters) {
+	if (!xdNode.stroke && !xdNode.hasRoundedCorners && !xdNode.shadow && xdNode.fill instanceof xd.Color) {
+		return _getFillParam(xdNode, serializer, ctx, parameters);
+	} else {
+		return _getDecorationParam(xdNode, serializer, ctx, parameters);
+	}
+}
+
+function _getDecorationParam(o, serializer, ctx, parameters) {
+	return `decoration: ${_getBoxDecoration(o, serializer, ctx, parameters)}, `;
+}
+
+function _getBoxDecoration(xdNode, serializer, ctx, parameters) {
+	let str = $.getParamList([
+		_getBorderRadiusParam(xdNode, serializer, ctx, parameters),
+		_getFillParam(xdNode, serializer, ctx, parameters),
+		_getBorderParam(xdNode, serializer, ctx, parameters),
+		_getBoxShadowParam(xdNode, serializer, ctx, parameters)
+	]);
+	return "BoxDecoration(" + str + ")";
+}
+
+/** FILL & STROKE */
+function _getFillParam(xdNode, serializer, ctx, parameters) {
+	if (!xdNode.fillEnabled || !xdNode.fill) { return ""; }
+	let fill = xdNode.fill, blur = xdNode.blur;
+	let fillOpacityFromBlur = (blur && blur.visible && blur.isBackgroundEffect) ? blur.fillOpacity : 1.0;
+	let opacity = NodeUtils.getOpacity(xdNode) * fillOpacityFromBlur;
+	if (fill instanceof xd.Color) {
+		let colorParameter = parameters["fill"].isOwn
+			? getColor(xdNode.fill, opacity)
+			: parameters["fill"].name;
+		return `color: ${colorParameter}, `;
+	}
+	if (fill instanceof xd.ImageFill) {
+		let imageParam = parameters["fill"].isOwn
+			? getAssetImage(xdNode, serializer, ctx)
+			: parameters["fill"].name;
+		return "image: DecorationImage("+
+			`  image: ${imageParam},` +
+			`  fit: ${getBoxFit(fill.scaleBehavior)},` +
+			_getOpacityColorFilterParam(opacity) +
+			"), ";
+	}
+	let gradient = getGradientParam(fill, opacity);
+	if (gradient) { return gradient; }
+	ctx.log.warn(`Unrecognized fill type ('${fill.constructor.name}').`, xdNode);
+}
+
+function _getOpacityColorFilterParam(opacity) {
+	if (opacity >= 1) { return ''; }
+	return `colorFilter: new ColorFilter.mode(Colors.black.withOpacity(${$.fix(opacity, 2)}), BlendMode.dstIn), `;
+}
+
+function _getBorderParam(xdNode, serializer, ctx, parameters) {
+	if (xdNode.strokePosition !== xd.GraphicNode.INNER_STROKE) {
+		ctx.log.warn('Only inner strokes are supported on rectangles & ellipses.', xdNode);
+	}
+	if (xdNode.strokeJoins !== xd.GraphicNode.STROKE_JOIN_MITER) {
+		ctx.log.warn('Only miter stroke joins are supported on rectangles & ellipses.', xdNode);
+	}
+	let dashes = xdNode.strokeDashArray;
+	if (dashes && dashes.length && dashes.reduce((a, b) => a + b)) {
+		ctx.log.warn('Dashed lines are not supported on rectangles & ellipses.', xdNode);
+	}
+	let strokeEnableParamRef = parameters["strokeEnabled"];
+	let strokeEnableParam = strokeEnableParamRef.parameter;
+	let strokeParam = parameters["stroke"].isOwn
+		? xdNode.stroke && getColor(xdNode.stroke, NodeUtils.getOpacity(xdNode))
+		: parameters["stroke"].name;
+	if (!strokeParam) { return ""; }
+
+	if (strokeEnableParamRef.isOwn) {
+		if (!xdNode.strokeEnabled || !xdNode.stroke) { return ""; }
+		return `border: Border.all(width: ${$.fix(xdNode.strokeWidth, 2)}, color: ${strokeParam}), `;
+	} else {
+		return `border: ${strokeEnableParam.name} ? Border.all(width: ${$.fix(xdNode.strokeWidth, 2)}, color: ${strokeParam}) : null, `;
+	}
+}
+
+
+/** BORDERRADIUS */
+function _getBorderRadiusParam(o, serializer, ctx) {
+	let radiusStr;
+	if (o instanceof xd.Ellipse) {
+		radiusStr = _getBorderRadiusForEllipse(o, serializer, ctx);
+	} else if (o.hasRoundedCorners) {
+		radiusStr = _getBorderRadiusForRectangle(o, serializer, ctx);
+	}
+	return radiusStr ? `borderRadius: ${radiusStr}, ` : "";
+}
+
+function _getBorderRadiusForEllipse(o, serializer, ctx) {
+	return `BorderRadius.all(Radius.elliptical(${$.fix(o.radiusX, 2)}, ${$.fix(o.radiusY, 2)}))`;
+}
+
+function _getBorderRadiusForRectangle(o, serializer, ctx) {
+	let radii = o.cornerRadii;
+	let tl = radii.topLeft, tr = radii.topRight, br = radii.bottomRight, bl = radii.bottomLeft;
+	if (tl === tr && tl === br && tl === bl) {
+		return `BorderRadius.circular(${$.fix(tl, 2)})`;
+	} else {
+		return 'BorderRadius.only(' +
+			_getRadiusParam("topLeft", tl) +
+			_getRadiusParam("topRight", tr) +
+			_getRadiusParam("bottomRight", br) +
+			_getRadiusParam("bottomLeft", bl) +
+		')';
+	}
+}
+
+function _getRadiusParam(param, value) {
+	if (value <= 1) { return ''; }
+	return `${param}: Radius.circular(${$.fix(value, 2)}), `;
+}
+
+
+/** SHADOWS */
+function _getBoxShadowParam(xdNode, serializer, ctx) {
+	let s = xdNode.shadow;
+	if (!s || !s.visible) { return ""; }
+	return "boxShadow: [BoxShadow(" +
+		`color: ${getColor(s.color, NodeUtils.getOpacity(xdNode))}, ` +
+		`offset: Offset(${s.x}, ${s.y}), ` +
+		`blurRadius: ${s.blur}, ` +
+	"), ], ";
+}
