8452642f0dc8c29bf9b5fecf3cb85145efa6c17b
Grant Skinner <info@gskinner.com>
2020-06-16 12:03:46 -0600

    Initial core responsive layout support

--- src/core/decorators/layout.js
+++ src/core/decorators/layout.js
@@ -17,28 +17,47 @@ const { NodeDecorator } = require("./nodedecorator");
 class Layout extends NodeDecorator {
 	static create(node, ctx) { throw("Layout.create() called."); }
 
+	constructor(node, ctx) {
+		super(node, ctx);
+		if (node.responsive) {
+			ctx.addImport("package:adobe_xd/pinned.dart");
+			ctx.addImport("dart:math"); // for Rectangle
+		}
+	}
+
 	_serialize(nodeStr, ctx) {
 		if (!this.node.layout) { return nodeStr; }
 		return (this.node.responsive) ? this._pin(nodeStr, ctx) : this._transform(nodeStr, ctx);
 	}
 
 	_pin(nodeStr, ctx) {
-		let xdNode = this.node.xdNode;
+		let xdNode = this.node.xdNode, C = xd.SceneNode;
+		let size = xdNode.parent.localBounds;
+		let bounds = this._getAdjustedBounds(xdNode, size);
+		let hConstraints = xdNode.horizontalConstraints, vConstraints = xdNode.verticalConstraints;
+		let hPos = hConstraints.position, hSize = hConstraints.size;
+		let vPos = vConstraints.position, vSize = vConstraints.size;
 
-		// TODO: GS: TEMPORARY:
-		return this._transform(nodeStr, ctx);
-
-		let bounds = xdNode.boundsInParent, size = xdNode.parent.localBounds;
-
-		return 'Pinned.fromSize(' +
-			`bounds: Rectangle(${bounds.x}, ${bounds.y}, ${bounds.width}, ${bounds.height}, ), ` +
-			`size: Size(${size.width}, ${size.height}), ` +
-
-		')';
+		return "Pinned.fromSize(" +
+			`bounds: Rectangle(${$.fix(bounds.x)}, ${$.fix(bounds.y)}, ${$.fix(bounds.width)}, ${$.fix(bounds.height)}), ` +
+			`size: Size(${$.fix(size.width)}, ${$.fix(size.height)}), ` +
+			(hPos === C.FIXED_LEFT || hPos === C.FIXED_BOTH ? "pinLeft: true, " : "") +
+			(hPos === C.FIXED_RIGHT || hPos === C.FIXED_BOTH ? "pinRight: true, " : "") +
+			(vPos === C.FIXED_TOP || vPos === C.FIXED_BOTH ? "pinTop: true, " : "") +
+			(vPos === C.FIXED_BOTTOM || vPos === C.FIXED_BOTH ? "pinBottom: true, " : "") +
+			(hSize === C.SIZE_FIXED ? "fixedWidth: true, " : "") +
+			(vSize === C.SIZE_FIXED ? "fixedHeight: true, " : "") +
+			`child: ${nodeStr}, ` +
+		")";
 			
 		// adjustTransform? rotation? Possibly wrap it in a transform?
 		// need to disable all the fixed sizes on Text, Rect, Ellipse, etc.
-		// add import.
+		// pinnedStack on Stack (and import it)
+	}
+
+	_getAdjustedBounds(xdNode, size) {
+		let bounds = xdNode.boundsInParent;
+		return {x: bounds.x - size.x, y: bounds.y - size.y, width: bounds.width, height: bounds.height};
 	}
 
 	_transform(nodeStr, ctx) {
@@ -55,16 +74,16 @@ class Layout extends NodeDecorator {
 			// Full transform matrix
 			// High precision for a-d, since rotation needs it.
 			nodeStr = "Transform(transform: Matrix4(" +
-				`${$.fix(mtx.a, 6)}, ${$.fix(mtx.b, 6)}, 0.0, 0.0, ` +
-				`${$.fix(mtx.c, 6)}, ${$.fix(mtx.d, 6)}, 0.0, 0.0, ` +
+				`${$.fix(mtx.a, 5)}, ${$.fix(mtx.b, 5)}, 0.0, 0.0, ` +
+				`${$.fix(mtx.c, 5)}, ${$.fix(mtx.d, 5)}, 0.0, 0.0, ` +
 				"0.0, 0.0, 1.0, 0.0, " +
-				`${$.fix(mtx.e, 2)}, ${$.fix(mtx.f, 2)}, 0.0, 1.0 ` +
+				`${$.fix(mtx.e)}, ${$.fix(mtx.f)}, 0.0, 1.0 ` +
 				`), child: ${nodeStr},` +
 			")";
 		} else if (mtx.e !== 0 || mtx.f !== 0) {
 			// Only translation
 			nodeStr = "Transform.translate(" +
-				`offset: Offset(${$.fix(mtx.e, 2)}, ${$.fix(mtx.f, 2)}), ` +
+				`offset: Offset(${$.fix(mtx.e)}, ${$.fix(mtx.f)}), ` +
 				`child: ${nodeStr},` +
 			")";
 		}
