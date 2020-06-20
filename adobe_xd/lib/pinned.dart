3e0ef3c64fd433e5c09f8bcda18cf3c6b98c7c22
Grant Skinner <info@gskinner.com>
2020-06-20 11:35:47 -0600

    Make Pinned centerFraction behavior match XD

--- adobe_xd/lib/pinned.dart
+++ adobe_xd/lib/pinned.dart
@@ -38,7 +38,7 @@ class Pinned extends StatelessWidget {
          end: pinRight ? size.width - bounds.right : null,
          startFraction: !pinLeft && !fixedWidth ? bounds.left / size.width : null,
          endFraction: !pinRight && !fixedWidth ? (size.width - bounds.right) / size.width : null,
-         centerFraction: fixedWidth && !pinLeft && !pinRight ? (bounds.left + bounds.width/2) / size.width : null,
+         centerFraction: fixedWidth && !pinLeft && !pinRight ? bounds.left / (size.width - bounds.width) : null,
        ),
        vPin = Pin(
          size: fixedHeight ? bounds.height : null,
@@ -46,32 +46,32 @@ class Pinned extends StatelessWidget {
          end: pinBottom ? size.height - bounds.bottom : null,
          startFraction: !pinTop && !fixedHeight ? bounds.top / size.height : null,
          endFraction: !pinBottom && !fixedHeight ? (size.height - bounds.bottom) / size.height : null,
-         centerFraction: fixedHeight && !pinTop && !pinBottom ? (bounds.top + bounds.height/2) / size.height : null,
+         centerFraction: fixedHeight && !pinTop && !pinBottom ? bounds.top / (size.height - bounds.height) : null,
        ),
        super(key: key);
 
   _Span calculateSpanFromPin(Pin pin, double maxSize) {
     double start = 0.0, end = 0.0;
     if (pin.size == null) {
-      //Size is unknown, so we must be pinned on both sides
+      // Size is unknown, so we must be pinned on both sides
       start = pin.start ?? pin.startFraction * maxSize;
       end = maxSize - (pin.end ?? pin.endFraction * maxSize);
     } else if (pin.size >= maxSize) {
-      //Exceeds max size, fill.
-      //Note: this isn't exactly what XD does, but it's the closest we can get without overflow.
+      // Exceeds max size, fill.
+      // Note: this isn't exactly what XD does, but it's the closest we can get without overflow.
       start = 0;
-      end = start + pin.size;
+      end = maxSize;
     } else if (pin.start != null || pin.startFraction != null) {
-      //Pinned to start
+      // Pinned to start
       start = min(maxSize - pin.size, pin.start ?? pin.startFraction * maxSize);
       end = start + pin.size;
     } else if (pin.end != null || pin.endFraction != null) {
-      //Pinned to end
+      // Pinned to end
       end = max(pin.size, maxSize - (pin.end ?? pin.endFraction * maxSize));
       start = end - pin.size;
     } else {
-      //Not pinned at all, use center to position
-      start = (pin.centerFraction * maxSize) - pin.size * .5;
+      // Not pinned at all, use center to position
+      start = pin.centerFraction * (maxSize - pin.size);
       end = start + pin.size;
     }
     return _Span(start, end);
@@ -79,12 +79,12 @@ class Pinned extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //Check to see if we have been provided some StackConstraints by [ PinnedStack ]
+    // Check to see if we have been provided some StackConstraints by [ PinnedStack ]
     StackConstraints constraints = context.dependOnInheritedWidgetOfExactType<StackConstraints>();
     if (constraints != null) {
       return _buildContent(constraints.constraints);
     }
-    //If not, we need to find our own constraints
+    // If not, we need to find our own constraints
     else {
       return LayoutBuilder(
         builder: (context, constraints) => _buildContent(constraints),
@@ -95,7 +95,7 @@ class Pinned extends StatelessWidget {
   Widget _buildContent(BoxConstraints constraints) {
     _Span hSpan = calculateSpanFromPin(hPin, constraints.maxWidth);
     _Span vSpan = calculateSpanFromPin(vPin, constraints.maxHeight);
-    //Hide child if either dimension is 0
+    // Hide child if either dimension is 0
     bool visible = (hSpan.size > 0 && vSpan.size > 0);
     return Padding(
       padding: EdgeInsets.fromLTRB(max(0.0, hSpan.start), max(0.0, vSpan.start), 0.0, 0.0),
