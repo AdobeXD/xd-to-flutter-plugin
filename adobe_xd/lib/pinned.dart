4fbf5e657d11f0f7423f5c64760eca0ef1b577eb
Grant Skinner <info@gskinner.com>
2020-06-30 10:26:55 -0600

    Clean up & document Pinned

--- adobe_xd/lib/pinned.dart
+++ adobe_xd/lib/pinned.dart
@@ -1,55 +1,102 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
 
-@immutable
-class Pin {
-  final double start;
-  final double startFraction;
-  final double end;
-  final double endFraction;
-  final double size;
-  final double centerFraction;
-
-  Pin({this.start, this.startFraction, this.end, this.endFraction, this.size, this.centerFraction});
-
-  String toString() {
-    return "Pin(start: $start, startFraction: $startFraction, end: $end, endFraction: $endFraction, size: $size, centerFraction: $centerFraction, )";
-  }
-}
-
 class Pinned extends StatelessWidget {
   final Pin hPin;
   final Pin vPin;
   final Widget child;
 
-  Pinned({Key key, this.hPin, this.vPin, this.child}) : super(key: key);
+  /// Positions and sizes a single child based on settings defined in horizontal and vertical
+  /// [Pin] instances. See the [Pin] documentation for details.
+  Pinned.fromPins(this.hPin, this.vPin, {this.child, Key key}) : super(key: key);
 
+  /// Constructs a Pinned instance by building horizontal & vertical [Pin] instances
+  /// from semantic parameters. For example, providing `left` & `width` parameters would
+  /// result in a `hPin` having `start` and `size` values.
+  Pinned({
+    Key key,
+    double left,
+    double leftFraction,
+    double right,
+    double rightFraction,
+    double width,
+    double horizontalMiddle,
+    double top,
+    double topFraction,
+    double bottom,
+    double bottomFraction,
+    double height,
+    double verticalMiddle,
+    Widget child,
+  }) : this.fromPins(
+            Pin(
+              start: left,
+              startFraction: leftFraction,
+              end: right,
+              endFraction: rightFraction,
+              size: width,
+              middle: horizontalMiddle,
+            ),
+            Pin(
+              start: top,
+              startFraction: topFraction,
+              end: bottom,
+              endFraction: bottomFraction,
+              size: height,
+              middle: verticalMiddle,
+            ),
+            child: child,
+            key: key);
+
+  /// Constructs a Pinned instance based on parameters that reflect the pinning mode exposed by Adobe XD:
+  ///
+  /// * `bounds` - the original boundaries of the child within its parent
+  /// * `size` - the original dimensions of the parent
+  /// * pin parameters that indicate the child should be pinned to an absolute position for that side
+  /// * parameters indicating if it should use a fixed width or height
+  ///
+  /// The first two values are used for calculating the initial position of the child within its parent.
+  /// That initial position is then used in conjunction with the other parameters to construct the `hPin`
+  /// and `vPin` objects used by Pinned.
   Pinned.fromSize({
     Key key,
-    @required Rectangle bounds, @required Size size,
-    bool pinLeft=false, bool pinRight=false,
-    bool pinTop=false, bool pinBottom=false,
-    bool fixedWidth=false, bool fixedHeight=false,
-    this.child,
-  }) : hPin = Pin(
-         size: fixedWidth ? bounds.width : null,
-         start: pinLeft ? bounds.left : null,
-         end: pinRight ? size.width - bounds.right : null,
-         startFraction: !pinLeft && !fixedWidth ? bounds.left / size.width : null,
-         endFraction: !pinRight && !fixedWidth ? (size.width - bounds.right) / size.width : null,
-         centerFraction: fixedWidth && !pinLeft && !pinRight ? bounds.left / (size.width - bounds.width) : null,
-       ),
-       vPin = Pin(
-         size: fixedHeight ? bounds.height : null,
-         start: pinTop ? bounds.top : null,
-         end: pinBottom ? size.height - bounds.bottom : null,
-         startFraction: !pinTop && !fixedHeight ? bounds.top / size.height : null,
-         endFraction: !pinBottom && !fixedHeight ? (size.height - bounds.bottom) / size.height : null,
-         centerFraction: fixedHeight && !pinTop && !pinBottom ? bounds.top / (size.height - bounds.height) : null,
-       ),
-       super(key: key);
+    @required Rectangle bounds,
+    @required Size size,
+    bool pinLeft = false,
+    bool pinRight = false,
+    bool pinTop = false,
+    bool pinBottom = false,
+    bool fixedWidth = false,
+    bool fixedHeight = false,
+    Widget child,
+  }) : this.fromPins(
+            Pin(
+              size: fixedWidth ? bounds.width : null,
+              start: pinLeft ? bounds.left : null,
+              end: pinRight ? size.width - bounds.right : null,
+              startFraction: !pinLeft && !fixedWidth ? bounds.left / size.width : null,
+              endFraction: !pinRight && !fixedWidth ? (size.width - bounds.right) / size.width : null,
+              middle: fixedWidth && !pinLeft && !pinRight ? bounds.left / (size.width - bounds.width) : null,
+            ),
+            Pin(
+              size: fixedHeight ? bounds.height : null,
+              start: pinTop ? bounds.top : null,
+              end: pinBottom ? size.height - bounds.bottom : null,
+              startFraction: !pinTop && !fixedHeight ? bounds.top / size.height : null,
+              endFraction: !pinBottom && !fixedHeight ? (size.height - bounds.bottom) / size.height : null,
+              middle: fixedHeight && !pinTop && !pinBottom ? bounds.top / (size.height - bounds.height) : null,
+            ),
+            child: child,
+            key: key);
 
-  _Span calculateSpanFromPin(Pin pin, double maxSize) {
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) => _buildContent(constraints),
+    );
+  }
+
+  _Span _calculateSpanFromPin(Pin pin, double maxSize) {
     double start = 0.0, end = 0.0;
     if (pin.size == null) {
       // Size is unknown, so we must be pinned on both sides
@@ -69,23 +116,16 @@ class Pinned extends StatelessWidget {
       end = max(pin.size, maxSize - (pin.end ?? pin.endFraction * maxSize));
       start = end - pin.size;
     } else {
-      // Not pinned at all, use center to position
-      start = pin.centerFraction * (maxSize - pin.size);
+      // Not pinned at all, use middle to position
+      start = pin.middle * (maxSize - pin.size);
       end = start + pin.size;
     }
     return _Span(start, end);
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constraints) => _buildContent(constraints),
-    );
-  }
-
   Widget _buildContent(BoxConstraints constraints) {
-    _Span hSpan = calculateSpanFromPin(hPin, constraints.maxWidth);
-    _Span vSpan = calculateSpanFromPin(vPin, constraints.maxHeight);
+    _Span hSpan = _calculateSpanFromPin(hPin, constraints.maxWidth);
+    _Span vSpan = _calculateSpanFromPin(vPin, constraints.maxHeight);
     // Hide child if either dimension is 0
     bool visible = (hSpan.size > 0 && vSpan.size > 0);
     return Padding(
@@ -100,11 +140,60 @@ class Pinned extends StatelessWidget {
   }
 }
 
+/// Defines a pinned layout along a single axis (horizontal or vertical). Used with (and by) the [Pinned]
+/// layout widget. In most cases, it's easiest to think of a Pin as defining the position of the starting
+/// and ending edges of the child. So, for a horizontal Pin, `start` maps to `left`, and `end` maps to `right`.
+///
+/// Each edge can be defined relative to the enclosing parent's dimensions, or relative to the opposite edge.
+/// Both absolute (eg. "pixel") values, and fractional (eg. "percentage") values can be used.
+///
+/// The `start` and `end` parameters specify a pixel inset from the parent dimensions.
+/// Ex. A horizontal pin defined as `Pin(start: 20.0, end: 30.0)`, laying out a child in a parent that is 200
+/// wide would position the child with its left edge at 20, and its right edge at 170 (ie. 200 - 30).
+///
+/// The `startFraction` and `endFraction` parameters specify a fractional inset from the parent dimensions.
+/// Ex. A horizontal pin defined as `Pin(startFraction: 0.15, endFraction: 0.2)`, laying out a child in a parent
+/// that is 200 wide would position the child with its left edge at 30 (ie. 0.15 * 200), and its right edge at 160
+/// (ie. 200 - 0.2 * 200).
+///
+/// The `size` parameter can be paired with either a single edge parameter (see above), or `middle` (see below) to
+/// set a pixel size for the child.
+/// Ex. a horizontal pin defined as `Pin(startFraction: 0.3, size: 70.0)` in a 200 wide parent, would position
+/// the child with its left edge at 60 (ie. 0.3 * 200), and right edge at 130 (ie. 60 + 70).
+///
+/// Finally, the `middle` parameter is paired with the `size` parameter to position the child based on a fraction
+/// of the remaining size in the parent.
+/// Ex. Given a horizontal pin defined as `Pin(size: 80.0, middle: 0.5)` in a parent 200 wide, the remaining size
+/// would be 120 (ie. 200 - 80). The left edge would be positioned at 60 (ie. 120 * 0.5), and the right edge at
+/// 240 (ie. 60 + 80), centering it in the available space.
+@immutable
+class Pin {
+  final double start;
+  final double startFraction;
+  final double end;
+  final double endFraction;
+  final double size;
+  final double middle;
+
+  Pin({this.start, this.startFraction, this.end, this.endFraction, this.size, this.middle})
+      : assert(!(start != null && startFraction != null), "Cannot have both start and startFraction values."),
+        assert(!(end != null && endFraction != null), "Cannot have both end and endFraction values."),
+        assert(!(middle != null && size == null), "A size value is required with a middle value."),
+        assert(!(middle != null && (start ?? startFraction ?? end ?? endFraction) != null),
+            "Only a size value can be used with a middle value."),
+        assert(!(size != null && (start ?? startFraction) != null && (end ?? endFraction) != null),
+            "Cannot have both start and end values when a size value is used.");
+
+  String toString() {
+    return "Pin(start: $start, startFraction: $startFraction, end: $end, endFraction: $endFraction, size: $size, middle: $middle, )";
+  }
+}
+
 @immutable
 class _Span {
   final double start;
   final double end;
-  
+
   _Span(this.start, this.end);
 
   double get size => max(0, end - start);
