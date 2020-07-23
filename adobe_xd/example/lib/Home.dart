720bd7588b4d2392d5b3f5bacf92a363b267bf9d
Grant Skinner <info@gskinner.com>
2020-07-23 11:47:00 -0600

    Update example

--- adobe_xd/example/lib/Home.dart
+++ adobe_xd/example/lib/Home.dart
@@ -332,75 +332,73 @@ class Home extends StatelessWidget {
                       ].map((map) {
                         final value = map['value'];
                         final label = map['label'];
-                        return SizedBox(
-                            width: 85,
-                            height: 48,
-                            child: Transform.translate(
-                              offset: Offset(-45.0, -439.5),
-                              child:
-                                  // Adobe XD layer: 'group' (group)
-                                  Stack(
-                                children: <Widget>[
-                                  Transform.translate(
-                                    offset: Offset(62.0, 448.0),
-                                    child:
-                                        // Adobe XD layer: 'numbers' (text)
-                                        SizedBox(
-                                      width: 47.0,
-                                      height: 20.0,
-                                      child: Text(
-                                        value,
-                                        style: TextStyle(
-                                          fontFamily: 'Helvetica',
-                                          fontSize: 20,
-                                          color: const Color(0xff00a9de),
-                                        ),
-                                        textAlign: TextAlign.center,
+                        return Transform.translate(
+                          offset: Offset(0.5, 0.5),
+                          child:
+                              // Adobe XD layer: 'group' (group)
+                              SizedBox(
+                            width: 85.0,
+                            height: 48.0,
+                            child: Stack(
+                              children: <Widget>[
+                                Transform.translate(
+                                  offset: Offset(16.5, 8.0),
+                                  child:
+                                      // Adobe XD layer: 'numbers' (text)
+                                      SizedBox(
+                                    width: 47.0,
+                                    height: 20.0,
+                                    child: Text(
+                                      value,
+                                      style: TextStyle(
+                                        fontFamily: 'Helvetica',
+                                        fontSize: 20,
+                                        color: const Color(0xff00a9de),
                                       ),
+                                      textAlign: TextAlign.center,
                                     ),
                                   ),
-                                  Transform.translate(
-                                    offset: Offset(62.0, 473.0),
-                                    child:
-                                        // Adobe XD layer: 'text' (text)
-                                        SizedBox(
-                                      width: 47.0,
-                                      height: 11.0,
-                                      child: Text(
-                                        label,
-                                        style: TextStyle(
-                                          fontFamily: 'Helvetica',
-                                          fontSize: 11,
-                                          color: const Color(0xffa2a2a2),
-                                          letterSpacing: 0.22,
-                                        ),
-                                        textAlign: TextAlign.center,
+                                ),
+                                Transform.translate(
+                                  offset: Offset(16.5, 33.0),
+                                  child:
+                                      // Adobe XD layer: 'text' (text)
+                                      SizedBox(
+                                    width: 47.0,
+                                    height: 11.0,
+                                    child: Text(
+                                      label,
+                                      style: TextStyle(
+                                        fontFamily: 'Helvetica',
+                                        fontSize: 11,
+                                        color: const Color(0xffa2a2a2),
+                                        letterSpacing: 0.22,
                                       ),
+                                      textAlign: TextAlign.center,
                                     ),
                                   ),
-                                  Transform.translate(
-                                    offset: Offset(128.5, 440.0),
-                                    child: Container(
-                                      width: 2.0,
-                                      height: 48.0,
-                                      decoration: BoxDecoration(
-                                        color: const Color(0xffececec),
-                                      ),
+                                ),
+                                Transform.translate(
+                                  offset: Offset(83.0, 0.0),
+                                  child: Container(
+                                    width: 2.0,
+                                    height: 48.0,
+                                    decoration: BoxDecoration(
+                                      color: const Color(0xffececec),
                                     ),
                                   ),
-                                  Transform.translate(
-                                    offset: Offset(45.5, 440.0),
-                                    child: Container(
-                                      width: 2.0,
-                                      height: 48.0,
-                                      decoration: BoxDecoration(
-                                        color: const Color(0xffececec),
-                                      ),
-                                    ),
+                                ),
+                                Container(
+                                  width: 2.0,
+                                  height: 48.0,
+                                  decoration: BoxDecoration(
+                                    color: const Color(0xffececec),
                                   ),
-                                ],
-                              ),
-                            ));
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
                       }).toList(),
                     ),
                   ),
