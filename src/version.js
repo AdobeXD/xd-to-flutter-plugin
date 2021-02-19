913029f9e9b4db23b6846db3d69429071060e32e
Grant Skinner <info@gskinner.com>
2021-02-19 13:59:03 -0700

    comment tweak in version file

--- src/version.js
+++ src/version.js
@@ -16,7 +16,7 @@ const { alert } = require("./ui/alert");
 // Update for new builds:
 exports.version = "1.1.0";
 exports.debug = false;
-exports.xdVersionRequired = 30; // the actual version required to run the plugin. Not 
+exports.xdVersionRequired = 30; // the actual minimum XD version required to run the plugin.
 
 // Calculated:
 exports.xdVersion = parseInt(app.version);
