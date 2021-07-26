d8ad9c48017dac0b341d2637ffda037234cb2aca
Grant Skinner <grant@gskinner.com>
2021-07-26 10:29:38 -0600

    Update example to use Normalize Names

--- adobe_xd/example/lib/home.dart
+++ adobe_xd/example/lib/home.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:adobe_xd/pinned.dart';
-import './Gear.dart';
+import './gear.dart';
 import 'package:adobe_xd/page_link.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -257,92 +257,94 @@ class Home extends StatelessWidget {
                         Pin(start: 3.0, end: 5.0),
                         Pin(size: 48.0, end: 11.5),
                         child:
-                            // Adobe XD layer: 'days, miles, camps,…' (unknown element)
-                            SingleChildScrollView(
-                          child: Wrap(
-                            alignment: WrapAlignment.center,
-                            spacing: 0,
-                            runSpacing: 20,
-                            children: [
-                              {
-                                'value': '10',
-                                'label': 'Days',
-                              },
-                              {
-                                'value': '160',
-                                'label': 'Miles',
-                              },
-                              {
-                                'value': '5',
-                                'label': 'Camps',
-                              },
-                              {
-                                'value': '2',
-                                'label': 'Bears',
-                              }
-                            ].map((itemData) {
-                              final value = itemData['value']!;
-                              final label = itemData['label']!;
-                              return SizedBox(
-                                width: 85.0,
-                                height: 48.0,
-                                child:
-                                    // Adobe XD layer: 'group' (group)
-                                    Stack(
-                                  children: <Widget>[
-                                    Pinned.fromPins(
-                                      Pin(size: 47.0, middle: 0.4342),
-                                      Pin(size: 20.0, middle: 0.2857),
-                                      child:
-                                          // Adobe XD layer: 'numbers' (text)
-                                          Text(
-                                        value,
-                                        style: TextStyle(
-                                          fontFamily: 'Helvetica',
-                                          fontSize: 20,
-                                          color: const Color(0xff00a9de),
-                                        ),
-                                        textAlign: TextAlign.center,
-                                      ),
-                                    ),
-                                    Pinned.fromPins(
-                                      Pin(size: 47.0, middle: 0.4342),
-                                      Pin(size: 11.0, end: 4.0),
-                                      child:
-                                          // Adobe XD layer: 'text' (text)
-                                          Text(
-                                        label,
-                                        style: TextStyle(
-                                          fontFamily: 'Helvetica',
-                                          fontSize: 11,
-                                          color: const Color(0xffa2a2a2),
-                                          letterSpacing: 0.22,
-                                        ),
-                                        textAlign: TextAlign.center,
-                                      ),
-                                    ),
-                                    Pinned.fromPins(
-                                      Pin(size: 2.0, end: 0.0),
-                                      Pin(start: 0.0, end: 0.0),
-                                      child: Container(
-                                        decoration: BoxDecoration(
-                                          color: const Color(0xffececec),
+                            // Adobe XD layer: 'days, miles, camps,…' (grid)
+                            Scrollbar(
+                          child: SingleChildScrollView(
+                            child: Wrap(
+                              alignment: WrapAlignment.center,
+                              spacing: 0,
+                              runSpacing: 20,
+                              children: [
+                                {
+                                  'value': '10',
+                                  'label': 'Days',
+                                },
+                                {
+                                  'value': '160',
+                                  'label': 'Miles',
+                                },
+                                {
+                                  'value': '5',
+                                  'label': 'Camps',
+                                },
+                                {
+                                  'value': '2',
+                                  'label': 'Bears',
+                                }
+                              ].map((itemData) {
+                                final value = itemData['value']!;
+                                final label = itemData['label']!;
+                                return SizedBox(
+                                  width: 85.0,
+                                  height: 48.0,
+                                  child:
+                                      // Adobe XD layer: 'group' (group)
+                                      Stack(
+                                    children: <Widget>[
+                                      Pinned.fromPins(
+                                        Pin(size: 47.0, middle: 0.4342),
+                                        Pin(size: 20.0, middle: 0.2857),
+                                        child:
+                                            // Adobe XD layer: 'numbers' (text)
+                                            Text(
+                                          value,
+                                          style: TextStyle(
+                                            fontFamily: 'Helvetica',
+                                            fontSize: 20,
+                                            color: const Color(0xff00a9de),
+                                          ),
+                                          textAlign: TextAlign.center,
                                         ),
                                       ),
-                                    ),
-                                    Pinned.fromPins(
-                                      Pin(size: 2.0, start: 0.0),
-                                      Pin(start: 0.0, end: 0.0),
-                                      child: Container(
-                                        decoration: BoxDecoration(
-                                          color: const Color(0xffececec),
+                                      Pinned.fromPins(
+                                        Pin(size: 47.0, middle: 0.4342),
+                                        Pin(size: 11.0, end: 4.0),
+                                        child:
+                                            // Adobe XD layer: 'text' (text)
+                                            Text(
+                                          label,
+                                          style: TextStyle(
+                                            fontFamily: 'Helvetica',
+                                            fontSize: 11,
+                                            color: const Color(0xffa2a2a2),
+                                            letterSpacing: 0.22,
+                                          ),
+                                          textAlign: TextAlign.center,
                                         ),
                                       ),
-                                    ),
-                                  ],
-                                ),
-                              );
-                            }).toList(),
+                                      Pinned.fromPins(
+                                        Pin(size: 2.0, end: 0.0),
+                                        Pin(start: 0.0, end: 0.0),
+                                        child: Container(
+                                          decoration: BoxDecoration(
+                                            color: const Color(0xffececec),
+                                          ),
+                                        ),
+                                      ),
+                                      Pinned.fromPins(
+                                        Pin(size: 2.0, start: 0.0),
+                                        Pin(start: 0.0, end: 0.0),
+                                        child: Container(
+                                          decoration: BoxDecoration(
+                                            color: const Color(0xffececec),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              }).toList(),
+                            ),
                           ),
                         ),
                       ),
@@ -372,7 +374,7 @@ class Home extends StatelessWidget {
                               child:
                                   // Adobe XD layer: 'ellipse' (shape)
                                   SvgPicture.string(
-                                _svg_6o6tx4,
+                                _svg_o6tx4,
                                 allowDrawingOutsideViewBox: true,
                                 fit: BoxFit.fill,
                               ),
@@ -508,7 +510,7 @@ class Home extends StatelessWidget {
                         child:
                             // Adobe XD layer: 'search icon' (shape)
                             SvgPicture.string(
-                          _svg_20idv2,
+                          _svg_idv2,
                           allowDrawingOutsideViewBox: true,
                           fit: BoxFit.fill,
                         ),
@@ -540,13 +542,13 @@ const String _svg_il6hf2 =
     '<svg viewBox="285.0 607.0 20.0 12.0" ><path transform="translate(281.0, 601.0)" d="M 9.110000610351562 6.539999961853027 C 9.460000038146973 5.860000133514404 10.01000022888184 5.809999942779541 10.39000034332275 6.46999979019165 C 11.42000007629395 8.289999008178711 13.05000019073486 11.88999652862549 13.83000087738037 13.39999580383301 C 14.19999885559082 14.11999797821045 14.65999984741211 14.13999843597412 15.10000038146973 13.37999629974365 C 15.76000118255615 12.26999759674072 17.21999931335449 9.95999813079834 17.95000267028809 8.709997177124023 C 18.32999992370605 8.049999237060547 19.01000213623047 8.060000419616699 19.42000007629395 8.69999885559082 C 20.67000007629395 10.65999603271484 23.03000259399414 15.07999610900879 23.8799991607666 16.67999839782715 C 24.2400016784668 17.35999488830566 23.7600040435791 17.99999618530273 23.05000305175781 17.99999618530273 L 4.989999294281006 17.99999618530273 C 4.28000020980835 17.99999618530273 3.839999675750732 17.3899974822998 4.059999942779541 16.63999557495117 C 4.59999942779541 14.77999591827393 7.780000686645508 9.069995880126953 9.110000610351562 6.539999961853027" fill="#ffffff" stroke="none" stroke-width="1" stroke-miterlimit="4" stroke-linecap="butt" /></svg>';
 const String _svg_uvjlj2 =
     '<svg viewBox="82.0 560.0 20.0 12.0" ><path transform="translate(78.0, 554.0)" d="M 9.110000610351562 6.539999961853027 C 9.460000038146973 5.860000133514404 10.01000022888184 5.809999942779541 10.39000034332275 6.46999979019165 C 11.42000007629395 8.289999008178711 13.05000019073486 11.88999652862549 13.83000087738037 13.39999580383301 C 14.19999885559082 14.11999797821045 14.65999984741211 14.13999843597412 15.10000038146973 13.37999629974365 C 15.76000118255615 12.26999759674072 17.21999931335449 9.95999813079834 17.95000267028809 8.709997177124023 C 18.32999992370605 8.049999237060547 19.01000213623047 8.060000419616699 19.42000007629395 8.69999885559082 C 20.67000007629395 10.65999603271484 23.03000259399414 15.07999610900879 23.8799991607666 16.67999839782715 C 24.2400016784668 17.35999488830566 23.7600040435791 17.99999618530273 23.05000305175781 17.99999618530273 L 4.989999294281006 17.99999618530273 C 4.28000020980835 17.99999618530273 3.839999675750732 17.3899974822998 4.059999942779541 16.63999557495117 C 4.59999942779541 14.77999591827393 7.780000686645508 9.069995880126953 9.110000610351562 6.539999961853027" fill="#ffffff" stroke="none" stroke-width="1" stroke-miterlimit="4" stroke-linecap="butt" /></svg>';
-const String _svg_6o6tx4 =
+const String _svg_o6tx4 =
     '<svg viewBox="0.5 0.5 22.0 22.0" ><path transform="translate(0.5, 0.5)" d="M 11 0 C 17.07513236999512 0 22 4.924868106842041 22 11 C 22 17.07513236999512 17.07513236999512 22 11 22 C 4.924868106842041 22 0 17.07513236999512 0 11 C 0 4.924868106842041 4.924868106842041 0 11 0 Z" fill="none" stroke="#808080" stroke-width="3" stroke-miterlimit="4" stroke-linecap="butt" /></svg>';
 const String _svg_emmxj3 =
     '<svg viewBox="7.2 7.1 8.3 8.3" ><path  d="M 7.599999904632568 7.099999904632568 C 7.299999713897705 7.099999904632568 7.099999904632568 7.299999713897705 7.199999809265137 7.5 C 7.699999809265137 9.800000190734863 8.300000190734863 13.10000038146973 9.600000381469727 14.39999961853027 C 11 15.79999923706055 13.20000076293945 15.79999923706055 14.5 14.39999961853027 C 15.79999923706055 13 15.89999961853027 10.79999923706055 14.5 9.5 C 13.19999980926514 8.300000190734863 9.899999618530273 7.599999904632568 7.599999904632568 7.099999904632568 Z M 13.10000038146973 13.10000038146973 C 12.5 13.70000076293945 11.60000038146973 13.70000076293945 11 13.10000038146973 C 10.39999961853027 12.5 10.39999961853027 11.60000038146973 11 11 C 11.60000038146973 10.39999961853027 12.5 10.39999961853027 13.10000038146973 11 C 13.60000038146973 11.5 13.60000038146973 12.5 13.10000038146973 13.10000038146973 Z" fill="#808080" stroke="none" stroke-width="1" stroke-miterlimit="4" stroke-linecap="butt" /></svg>';
 const String _svg_dzwujb =
     '<svg viewBox="144.0 168.5 88.0 1.0" ><path transform="translate(144.0, 168.5)" d="M 0 0 L 88 0" fill="none" stroke="#00a9de" stroke-width="2" stroke-miterlimit="4" stroke-linecap="butt" /></svg>';
-const String _svg_20idv2 =
+const String _svg_idv2 =
     '<svg viewBox="340.0 38.0 22.0 22.0" ><path transform="translate(150.0, -699.0)" d="M 204.3553771972656 739.3668212890625 C 207.0109252929688 739.9168701171875 209.1165161132812 742.00732421875 209.6743621826172 744.64794921875 C 210.7821807861328 749.59912109375 206.6809692382812 753.89013671875 201.809814453125 753.22216796875 C 198.5964202880859 752.8919677734375 196.04296875 750.25927734375 195.60302734375 747.1785888671875 C 195.0451812744141 742.3375244140625 199.3663787841797 738.2667236328125 204.3553771972656 739.3668212890625 L 204.3553771972656 739.3668212890625 Z M 211.9999542236328 746.7384033203125 C 211.9999542236328 745.96826171875 211.9999542236328 745.1981201171875 211.7799682617188 744.4281005859375 C 211.1121368408203 740.797119140625 208.1187591552734 737.9364013671875 204.4653778076172 737.166259765625 C 198.5964202880859 736.0660400390625 193.3874206542969 740.5770263671875 193.3874206542969 746.2984619140625 C 193.3874206542969 748.3887939453125 194.0473785400391 750.25927734375 195.2730102539062 751.799560546875 C 193.3874206542969 753.662109375 190.7239837646484 756.1927490234375 190.5039825439453 756.4127197265625 C 190.1739959716797 756.8529052734375 189.6161804199219 757.8431396484375 190.3939971923828 758.61328125 C 191.1717987060547 759.3834228515625 192.1696166992188 758.8331298828125 192.6095886230469 758.5032958984375 L 197.3707733154297 753.772216796875 C 198.9263763427734 754.8724365234375 200.8119812011719 755.532470703125 202.9176025390625 755.532470703125 C 207.6787719726562 755.532470703125 211.7799682617188 751.6895751953125 211.9999542236328 746.7384033203125 L 211.9999542236328 746.7384033203125 Z" fill="#ffffff" stroke="#000000" stroke-width="1" stroke-opacity="0.0" stroke-miterlimit="4" stroke-linecap="butt" /></svg>';
 const String _svg_p45gdo =
     '<svg viewBox="14.0 38.0 28.0 21.0" ><path transform="translate(14.1, 38.0)" d="M 13.89999961853027 0 C 13.20496368408203 0 12.80780029296875 0.4057970941066742 12.60921859741211 0.7101449370384216 L 3.474467992782593 17.95652198791504 L 1.488652467727661 17.95652198791504 C 0.5950354337692261 17.95652198791504 -0.1000000238418579 18.66666793823242 -0.1000000238418579 19.47826194763184 C -0.1000000238418579 20.28985595703125 0.5950353741645813 21 1.488652467727661 21 L 4.368085384368896 21 L 23.43191337585449 21 L 26.31134605407715 21 C 27.20496368408203 21 27.89999961853027 20.28985595703125 27.89999961853027 19.47826194763184 C 27.89999961853027 18.66666793823242 27.20496368408203 17.95652198791504 26.31134605407715 17.95652198791504 L 24.32553100585938 17.95652198791504 L 15.19077968597412 0.7101449370384216 C 14.99219799041748 0.4057970941066742 14.5950345993042 0 13.89999961853027 0 L 13.89999961853027 0 Z M 13.89999961853027 7.507246494293213 L 19.06311988830566 17.95652198791504 L 8.736879348754883 17.95652198791504 L 13.89999961853027 7.507246494293213 Z" fill="#ffffff" stroke="none" stroke-width="1" stroke-miterlimit="4" stroke-linecap="butt" /></svg>';
--- adobe_xd/example/lib/main.dart
+++ adobe_xd/example/lib/main.dart
@@ -1,4 +1,4 @@
-import 'Home.dart';
+import 'home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
