083d31dde69016405e4546732047bfd6f71efc27
Grant Skinner <grant@gskinner.com>
2021-11-29 09:57:25 -0700

    Make getAlignment public

--- src/utils/exportutils.js
+++ src/utils/exportutils.js
@@ -80,11 +80,17 @@ function getScrollView(childStr, node, ctx) {
 }
 exports.getScrollView = getScrollView;
 
+function getAlignment(x, y) {
+	// XD uses 0 to 1, Flutter uses -1 to +1.
+	return `Alignment(${$.fix(x*2-1, 3)}, ${$.fix(y*2-1, 3)})`;
+}
+exports.getAlignment = getAlignment;
+
 
 function _getLinearGradient(fill, opacity=1) {
 	return 'LinearGradient('+
-		`begin: ${_getAlignment(fill.startX, fill.startY)},` +
-		`end: ${_getAlignment(fill.endX, fill.endY)},` +
+		`begin: ${getAlignment(fill.startX, fill.startY)},` +
+		`end: ${getAlignment(fill.endX, fill.endY)},` +
 		_getColorsParam(fill.colorStops, opacity) +
 	')';
 }
@@ -93,9 +99,9 @@ function _getRadialGradient(fill, opacity=1) {
 	// Flutter always draws relative to the shortest edge, whereas XD draws the gradient
 	// stretched to the aspect ratio of its container.
 	return 'RadialGradient('+
-		`center: ${_getAlignment(fill.startX, fill.startY)}, ` +
+		`center: ${getAlignment(fill.startX, fill.startY)}, ` +
 		(fill.startX === fill.endX && fill.startY === fill.endY ? '' :
-			`focal: ${_getAlignment(fill.endX, fill.endY)}, `) +
+			`focal: ${getAlignment(fill.endX, fill.endY)}, `) +
 		(fill.startR === 0 ? '' : `focalRadius: ${$.fix(fill.startR, 3)}, `) +
 		`radius: ${$.fix(fill.endR, 3)}, ` +
 		_getColorsParam(fill.colorStops, opacity) +
@@ -109,7 +115,7 @@ function _getSweepGradient(fill, opacity=1) {
 	// As such, we need to rotate & move the gradient via GradientXDTransform
 	
 	return 'SweepGradient('+
-		`center: ${_getAlignment(fill.startX, fill.startY)}, ` +
+		`center: ${getAlignment(fill.startX, fill.startY)}, ` +
 		`startAngle: 0.0, endAngle: ${$.fix(Math.PI*2, 4)}, ` +
 		_getColorsParam(fill.colorStops, opacity) +
 		_getTransformParam(fill, _getRotationMtx(fill.rotation / 180 * Math.PI)) +
@@ -132,7 +138,7 @@ function _getTransformParam(fill, mtx) {
 	return 'transform: GradientXDTransform(' +
 		`${$.fix(o.a, 3)}, ${$.fix(o.b, 3)}, ${$.fix(o.c, 3)}, ` +
 		`${$.fix(o.d, 3)}, ${$.fix(o.e, 3)}, ${$.fix(o.f, 3)}, ` +
-		`${_getAlignment(fill.startX, fill.startY)}), `;
+		`${getAlignment(fill.startX, fill.startY)}), `;
 }
 
 function _getRotationMtx(r) {
@@ -140,11 +146,6 @@ function _getRotationMtx(r) {
 		a: Math.cos(r), b: Math.sin(r), e: 0,
 		c:-Math.sin(r), d: Math.cos(r), f: 0,
 	}
-}
-
-function _getAlignment(x, y) {
-	// XD uses 0 to 1, Flutter uses -1 to +1.
-	return `Alignment(${$.fix(x*2-1, 2)}, ${$.fix(y*2-1, 2)})`;
 }
 
 function _getScrollDirectionParam(node, ctx) {
